b13bfa47d12509dfa75f43095d08f5350c8e8e9a
Eonji-sw <ejkim0625@gmail.com>
2022-08-26 18:10:20 +0900

    feat: 인증번호 중복 생성 차단

--- connect_device.py
+++ connect_device.py
@@ -3,26 +3,33 @@ import uuid
 import random
 import firebase_db
 
+f = firebase_db.FirebaseDB()
+
+total_dict = f.get()  # DB 전체 데이터
+total_key = sorted(total_dict.keys())  # DB 정렬된 키
+
 device_id = str(uuid.uuid4())  # 디바이스 id
 certification_no = ""  # 인증번호
-for i in range(6):
-    certification_no += str(random.randrange(0, 10))
 response = False  # 인증 상태값
 
-
-f = firebase_db.FirebaseDB()
+# 인증번호 생성
+for i in range(6):
+    certification_no += str(random.randrange(0, 10))
+# 인증번호 중복 차단
+while certification_no in total_key:
+    certification_no = ""
+    for i in range(6):
+        certification_no += str(random.randrange(0, 10))
 
 f.update({certification_no: {  # 인증번호 컬렉션
     "id": device_id,
     "state": response}
 })
 
-print("id: ", device_id)  # 확인용 코드
-print("no: ", certification_no)  # 확인용 코드
+print("id: ", device_id)  # 테스트용 코드
+print("no: ", certification_no)  # 테스트용 코드
 
-input_no = input()
-total_dict = f.get()
-total_key = sorted(total_dict.keys())
+input_no = input()  # 테스트용 코드
 
 if input_no == certification_no:
     # if response == True:
